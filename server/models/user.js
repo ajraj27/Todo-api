11fdbe61379044d3b2c50f66ef021fe0ba1b1ef5
ajraj27 <rajanuj.iiita@gmail.com>
2018-08-20 14:33:07 +0530

    Added logout route of a user

--- server/models/user.js
+++ server/models/user.js
@@ -55,6 +55,17 @@ UserSchema.methods.getAuthToken=function(){
   });
 };
 
+UserSchema.methods.removeToken=function(token){
+  const user=this;
+
+  return user.update({
+    $pull:{
+      tokens:{token}
+    }
+  });
+
+};
+
 UserSchema.statics.findByToken=function(token){
   const User=this;
   let decoded;
