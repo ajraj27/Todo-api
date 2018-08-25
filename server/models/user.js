68c96e1f122a0d5182af47afbe8ad885f8f89f77
ajraj27 <rajanuj.iiita@gmail.com>
2018-08-25 15:12:07 +0530

    Made changes in secret key

--- server/models/user.js
+++ server/models/user.js
@@ -46,7 +46,7 @@ UserSchema.methods.toJSON=function(){
 UserSchema.methods.getAuthToken=function(){
   const user=this;
   const access='auth';
-  const token=jwt.sign({_id:user._id.toHexString(),access},'123abc').toString();
+  const token=jwt.sign({_id:user._id.toHexString(),access},process.env.JWT_SECRET).toString();
 
   user.tokens.push({access,token});
 
@@ -71,7 +71,7 @@ UserSchema.statics.findByToken=function(token){
   let decoded;
 
   try{
-    decoded=jwt.verify(token,'123abc');
+    decoded=jwt.verify(token,process.env.JWT_SECRET);
   } catch (e){
     return Promise.reject();
   }
